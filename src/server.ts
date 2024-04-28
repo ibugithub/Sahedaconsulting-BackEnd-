a005cc0523f37953af1a8fe0554d67b233ab27fc
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-28 19:08:49 +0600

    checking...

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 console.log('upload path***** ',(path.join(__dirname, '../uploads')));
+console.log('alter upload path***** ',(path.join(__dirname, '../../uploads')));
 app.use('/api/users', userRoutes);
 app.use('/api/prod', productRoutes);
 
