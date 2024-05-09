46bdfa59dae662a6074a3f47d34ff00950e963f8
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-05-09 16:48:56 +0600

    Fix user.ts file

--- src/features/users.ts
+++ src/features/users.ts
@@ -7,7 +7,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "";
 
 export const refreshTokenF = (req:Request, res:Response) => {
   try {
-    const refreshToken = req.cookies.refreshToken;
+    const refreshToken = req.body.refreshToken;
     const decodeToken = jwt.verify(refreshToken, JWT_SECRET) as JwtPayload
     const accessToken = generateAccessToken({usereId: decodeToken.userId, email: decodeToken.email})
     return res.status(200).json({message: 'New Access Token has been created', accessToken: accessToken})
