a8d12d4972f77d00cf74f8c17b0d674b7033f0eb
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-10-06 06:32:56 +0600

    Delete some unwanted sudden files

--- src/Utils/clearCookie.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clearCookie = void 0;
-const clearCookie = (res, cookieName) => {
-    res.clearCookie(cookieName);
-    return res.status(200).json({ message: 'cookie cleared successfully' });
-};
-exports.clearCookie = clearCookie;
--- src/controllers/buyerController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendMailC = void 0;
-const sendMail_1 = require("../features/sendMail");
-const sendMailC = (req, res) => {
-    (0, sendMail_1.sendMailF)(req, res);
-};
-exports.sendMailC = sendMailC;
